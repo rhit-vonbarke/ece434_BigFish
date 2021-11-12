41ae11e99356db72b45b641e4d7fe4532dbdbe5f
Kurtis VonBargen <vonbarke@rose-hulman.edu>
2021-11-12 21:45:41 +0000

    created install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing necessary libaries, this may be slow:"
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo apt install software-properties-common
+sudo apt update
+sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+sudo apt install ffmpeg
+mkdir "audiodownloads"
+#test code; comment out later
+youtube-dl -x --audio-format mp3 https://www.youtube.com/watch?v=diSjU2Go1mM
