bb71b95ea66715f80f003af3803835b21b0c03b1
fernerme <mferner2001@gmail.com>
2021-11-14 21:19:28 +0000

    merge IO with processor

--- wavprocessor.py
+++ wavprocessor.py
@@ -5,6 +5,7 @@ import sys
 import bigfish_io as bfio
 from scipy.fftpack import fft
 from scipy.io import wavfile
+import time 
 
 # map the FFT output to 2 matrices each with 8 columns of height 8
 # FFT ranges from -1 to 1
@@ -12,17 +13,24 @@ def map_to_LED_matrix(input_array):
     matrix_lo = [0 for r in range(8)]
     matrix_hi = [0 for r in range(8)]
     stepsize = len(input_array) // 16
+    #for i in range(len(input_array)):
+        #if input_array[i] > 1:
+            #print ('too large value: ' + str(input_array[i]))
     # get the average value of each subsection of the FFT array, sans the last
     for i in range(15):
-        raw_avg = avg_value_in_range(input_array, i * stepsize, (i + 1) * stepsize)
-        fix_avg = (raw_avg + 1) * 8 // 2
+        raw_avg = max_value_in_range(input_array, i * stepsize, (i + 1) * stepsize)
+        #print (raw_avg)
+        fix_avg = raw_avg * 8
+        #print (fix_avg)
         if(i < 8):
-            matrix_lo[i] = fix_avg
+            matrix_lo[i] = int(fix_avg)
         else:
-            matrix_hi[i-8] = fix_avg
-    raw_avg = avg_value_in_range(input_array, 15 * stepsize, len(input_array) - 1)
+            matrix_hi[i-8] = int(fix_avg)
+    raw_avg = max_value_in_range(input_array, 15 * stepsize, len(input_array) - 1)
     fix_avg = (raw_avg + 1) * 8 // 2
-    matrix_hi[7] = fix_avg
+    matrix_hi[7] = int(fix_avg)
+    #print (matrix_lo)
+    #print (matrix_hi)
     bfio.updateMatrix(bfio.LOW, matrix_lo)
     bfio.updateMatrix(bfio.HIGH, matrix_hi)
 
@@ -36,10 +44,18 @@ def avg_value_in_range(input_array, start, end):
     print (sum)
     return sum
 
+def max_value_in_range(input_array, start, end):
+    max = -sys.maxsize - 1
+    for i in range(start, end):
+        if (input_array[i] > max):
+            max = input_array[i]
+    return max
+    
+
 def processaudio(filename):
     refreshrate = 10 #Hz
     bitdepth = 8
-    downsample_ratio = 8
+    downsample_ratio = 4
     #divisor = 2**bitdepth
     parentpath = os.getcwd()
     filepath = (parentpath + '/audiodownloads/' + filename +'.wav')
@@ -51,24 +67,26 @@ def processaudio(filename):
     print ('Number of samples: ' + str(numsamples))
     #for i in range(numsamples):
     #    a[i] = (raw[i]/2**bitdepth) * 2 - 1 #normalize to the range [-1,1]
-    raw = raw[::downsample_ratio]
-    a=[(ele/2**8.)*2-1 for ele in raw]
+    a = raw[::downsample_ratio]
+    #a=[(ele/2**8.)*2-1 for ele in raw]
     print('Downsampled length: ' + str(len(a)))
     tracklength = int(numsamples / fs)
     chunklength = int(fs / refreshrate)
     print ('Track length (s): ' + str(tracklength))
     print ('Samples per chunk: ' + str(chunklength))
     startindex = 0
+    input('ready; press enter to start')
     while (startindex + chunklength) < numsamples:
         chunk = a[startindex:(startindex + chunklength)]
         res = fft(chunk)
         spectrum = res[:len(res)//2 - 1]
-        print(len(spectrum))
-        spectrum = [abs(ele) for ele in spectrum]
+        #print('Length of spectrum: '+ str(len(spectrum)))
+        spectrum = [abs(ele)/numsamples for ele in spectrum]
         map_to_LED_matrix(spectrum)
         startindex += chunklength
         #print(len(spectrum))
         #print(str(len(spectrum)) + '\n' + str(spectrum))
+        time.sleep(1/refreshrate)
 
 
 def main():
