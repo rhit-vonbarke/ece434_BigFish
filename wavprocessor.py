4dc1dc35c0b9f4a0ff8b8ada8594c2e086b9a65f
Kurtis VonBargen <vonbarke@rose-hulman.edu>
2021-11-14 20:18:39 +0000

    implement matrices into wavprocessor

--- wavprocessor.py
+++ wavprocessor.py
@@ -2,35 +2,39 @@
 
 import os
 import sys
+import bigfish_io as bfio
 from scipy.fftpack import fft
 from scipy.io import wavfile
 
 # map the FFT output to 2 matrices each with 8 columns of height 8
 # FFT ranges from -1 to 1
 def map_to_LED_matrix(input_array):
-    matrix_lo = [[0 for c in range(8)] for r in range(8)]
-    matrix_hi = [[0 for c in range(8)] for r in range(8)]
+    matrix_lo = [0 for r in range(8)]
+    matrix_hi = [0 for r in range(8)]
     stepsize = len(input_array) // 16
     # get the average value of each subsection of the FFT array, sans the last
     for i in range(15):
         raw_avg = avg_value_in_range(input_array, i * stepsize, (i + 1) * stepsize)
         fix_avg = (raw_avg + 1) * 8 // 2
         if(i < 8):
-            set_LED_col_height(matrix_lo, i, fix_avg)
+            matrix_lo[i] = fix_avg
         else:
-            set_LED_col_height(matrix_hi, i-8, fix_avg)
+            matrix_hi[i-8] = fix_avg
     raw_avg = avg_value_in_range(input_array, 15 * stepsize, len(input_array) - 1)
     fix_avg = (raw_avg + 1) * 8 // 2
-    set_LED_col_height(matrix_hi, 7, fix_avg)
+    matrix_hi[7] = fix_avg
+    bfio.updateMatrix(bfio.LOW, matrix_lo)
+    bfio.updateMatrix(bfio.HIGH, matrix_hi)
 
 
 def avg_value_in_range(input_array, start, end):
-    return(0)
-
-# TODO: make color functional with green on the bottom 5, yellow on the next two, red on top
-def set_LED_col_height(input_matrix, col, height):
-    for r in range(8-height, 8):
-        input_matrix[r][col] = 1
+    sum = 0
+    div = end-start
+    for i in range(start, end):
+        sum += input_array[i]
+    sum /= div
+    print (sum)
+    return sum
 
 def processaudio(filename):
     refreshrate = 10 #Hz
@@ -61,12 +65,15 @@ def processaudio(filename):
         spectrum = res[:len(res)//2 - 1]
         print(len(spectrum))
         spectrum = [abs(ele) for ele in spectrum]
+        map_to_LED_matrix(spectrum)
         startindex += chunklength
-        print(len(spectrum))
+        #print(len(spectrum))
         #print(str(len(spectrum)) + '\n' + str(spectrum))
 
 
 def main():
+    bfio.initGPIO()
+    bfio.clearMatrices()
     print (sys.argv[1])
     processaudio(sys.argv[1])
 
