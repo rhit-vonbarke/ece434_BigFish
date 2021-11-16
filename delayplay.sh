9f0c1f536d2f4be8d95a823d88e2fc75c368352b
rhit-vonbarke <54991315+rhit-vonbarke@users.noreply.github.com>
2021-11-15 19:00:20 -0500

    Update delayplay.sh

--- delayplay.sh
+++ delayplay.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-#x=`date +%s%N`
-#echo $x
-#y=`expr $x / 1000000000`
-#echo $y
-
-#accurate* sleep protocol from common time start
 timegoal=`expr $2 + 10000000000`
-#echo $2
-#echo $timegoal
 x=`date +%s%N`
 while [ $x -lt $timegoal ]
 do
 x=`date +%s%N`
-#echo $x
 done
 
 aplay audiodownloads/${1}.wav
